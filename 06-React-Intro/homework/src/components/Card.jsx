20a4a0521cc99840a3ddc7ac68bab0db9cd3ccde
elgela <gelatomarcelo@hotmail.com>
2022-12-20 01:50:07 -0300

    continuado

--- 06-React-Intro/homework/src/components/Card.jsx
+++ 06-React-Intro/homework/src/components/Card.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Card({max, min, name, img, onClose}) {
   // acá va tu código
  return (
-  <div>
+  <>
     <button onClick={onClose}>x</button>
     <h3>{name}</h3>
     <h5>Min</h5>
@@ -11,6 +11,6 @@ export default function Card({max, min, name, img, onClose}) {
     <h5>Max</h5>
     <div>{max}</div>
     <img src={`http://openweathermap.org/img/wn/${img}@2x.png`} alt=''/>
-  </div>
+  </>
   )
 };
--- 06-React-Intro/homework/src/components/Cards.jsx
+++ 06-React-Intro/homework/src/components/Cards.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import Card from './Card';
 
-export default function Cards({data}) {
+export default function Cards({cities}) {
   // acá va tu código
   // tip, podés usar un map
   return (
-  data.map(ciudad =>
-    <Card />
+  cities?.map(ciudad =>
+    <Card
+    key={ciudad.id }
+    max={ciudad.main.temp_max}
+    min={ciudad.main.temp_min}
+    name={ciudad.name}
+    img={ciudad.weather[0].icon}
+    onClose={() => alert('que mirá bobo, andá pa`lla bobo')}
+    />
     )
   )
 };
