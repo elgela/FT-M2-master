e45ac417935ff4ccac7bd82e60c7ff9a91e593ef
elgela <gelatomarcelo@hotmail.com>
2023-01-01 12:35:41 -0300

    completo con extras

--- 03-ES6/homework/src/selector.js
+++ 03-ES6/homework/src/selector.js
@@ -24,13 +24,16 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
 
 var selectorTypeMatcher = function(selector) {
   // tu código aquí
-  if(selector[0] === '#') return 'id';
-  if(selector[0] === '.') return 'class';
-  // if(selector.split('.').length === 2) return 'tag.class';
+  const firstChar = selector[0]
+  if(firstChar === '#') return 'id';
+  if(firstChar === '.') return 'class';
+  if(selector.includes('.')) return 'tag.class';
           // o si no con un 'for'
-  for (let i = 0; i < selector.length; i++) {
-      if(selector[i] === '.') return 'tag.class';
-  }
+  // for (let i = 0; i < selector.length; i++) {
+  //     if(selector[i] === '.') return 'tag.class';
+  // }
+  if(selector.includes('>')) return 'child combinator';
+  if(!selector.includes('>') && selector.includes(' ')) return 'descendant combinator'
     return 'tag';
 };
 
@@ -39,42 +42,68 @@ var selectorTypeMatcher = function(selector) {
 // parametro y devuelve true/false dependiendo si el elemento
 // matchea el selector.
 
-var matchFunctionMaker = function(selector) {
-  var selectorType = selectorTypeMatcher(selector);
-  var matchFunction;
-  if (selectorType === "id") { 
-   matchFunction = function(el){
-    return '#' + el.id === selector;
-   }
-  } else if (selectorType === "class") {
-    matchFunction = function(el){
-      if(el.classList.length > 1){
-        for(let i = 0; i < el.classList.length; i++){
-          if('.' + el.classList[i] === selector){
-            return true;
+const matchFunctionMaker = function(selector) {
+  const selectorType = selectorTypeMatcher(selector);
+
+  let matchFunction;
+  if (selectorType === 'id') {
+    matchFunction = function (element) {
+      return element.id === selector.slice(1);
+    };
+  } else if (selectorType === 'class') {
+    matchFunction = function (element) {
+      const classes = element.className.split(' ');
+      return classes.includes(selector.slice(1));
+    };
+  } else if (selectorType === 'tag.class') {
+    matchFunction = function (element) {
+
+      [tag, theClass] = selector.split('.'); // ahora tenemos una variable llamada 'tag' y ninguna llamada 'theClass'
+
+      const classes = element.className.split(' ');
+      return classes.includes(theClass) &&
+             element.tagName.toLowerCase() === tag.toLowerCase();
+    };
+  } else if (selectorType === 'tag') {
+    matchFunction = function (el) {
+      return el.tagName && (el.tagName.toLowerCase() === selector.toLowerCase());
+    };
+  } else if (selectorType === "child combinator") {
+    // for Selector Hierarchy extra credit
+    const selectorArr = selector.split(' > ');
+    const [parentSelector, childSelector] = selectorArr;
+
+    matchFunction = function (element) {
+      return matchFunctionMaker(parentSelector)(element.parentElement) && matchFunctionMaker(childSelector)(element)
+    };
+  } else if (selectorType === "descendant combinator") {
+    // also for Selector Hierarchy extra credit
+    const selectorArr = selector.split(' ');
+    const [ancestorSelector, elementSelector] = selectorArr;
+
+    matchFunction = function (element) {
+      let ancestorBool;
+
+      function walkUpTheDOM(el) {
+        el = el.parentElement;
+        while (el) {
+          if (el.nodeName == "#text") el = el.nextSibling;
+          if (matchFunctionMaker(ancestorSelector)(el)) {
+            ancestorBool = true;
+            break;
           }
+
+          walkUpTheDOM(el);
+          el = el.nextSibling;
         }
       }
-      return '.' + el.classList === selector;
-    }
-  } else if (selectorType === "tag.class") {
-    matchFunction = function(el){
-      let array = selector.split('.');
-      if(el.tagName.toLowerCase() === array[0]){
-        if(el.classList.length > 1){
-          for (let i = 0; i < el.classList.length; i++) {
-            if(el.classList[i] === array[1]) return true;
-          }
-        }
-        if(el.classList === array[1]) return true;
-      }
-      return false;
-    }
-  } else if (selectorType === "tag") {
-    matchFunction = function(el){
-      return el.tagName.toLowerCase() === selector;
+
+      walkUpTheDOM(element);
+
+      return ancestorBool && matchFunctionMaker(elementSelector)(element);
     }
   }
+
   return matchFunction;
 };
 
@@ -82,12 +111,13 @@ var $ = function(selector) {
   var elements;
   var selectorMatchFunc = matchFunctionMaker(selector);// funcion que recibe un elemento y recibe T o F
   elements = traverseDomAndCollectElements(selectorMatchFunc);
+  
   return elements;
 };
 
-var extraCredit = function(selector) {
-  if(selector('>')){
-    return selector;
-  }
+// var extraCredit = function(selector) {
+//   if(selector === '>' || selector === ' '){
+    
+//   } return elements;
  
-}
+// }
