a60829085d41ff3687a0c5273b125978715258fd
Marcelo <gelatomarcelo@hotmail.com>
2023-01-04 21:24:08 -0300

    Empezado

--- 07-React-Estilos/demo/cssStylesheet/app.js
+++ 07-React-Estilos/demo/cssStylesheet/app.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Producto from './src/components/Product.jsx';
 import OtroComponente from './src/components/OtroComponente.js';
+import Producto from './src/components/Product.jsx';
 
-render(<div>
-    <Producto title="Prueba" price={400}/>
-    <OtroComponente title="Otro" price={133}/>
-    </div>, document.getElementById('app'));
+render(
+    <div>
+        <Producto title="Prueba" price={400}/>
+        <OtroComponente title="Otro" price={133}/>
+    </div>
+    , document.getElementById('app'));
