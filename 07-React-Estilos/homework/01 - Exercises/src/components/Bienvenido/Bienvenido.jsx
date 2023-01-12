e3b377e17edcdd2aa8762c9e72d9f7226940b30d
elgela <gelatomarcelo@hotmail.com>
2023-01-12 00:05:44 -0300

    incompleto

--- 07-React-Estilos/homework/01 - Exercises/src/components/Bienvenido/Bienvenido.jsx
+++ 07-React-Estilos/homework/01 - Exercises/src/components/Bienvenido/Bienvenido.jsx
@@ -5,7 +5,7 @@ import cssImg from '../../assets/css.svg'
 import javascriptImg from '../../assets/javascript.png'
 import reactImg from '../../assets/react.png'
 import redux from '../../assets/redux.png'
-// import styles from './Bienvenido.module.css'
+import styles from './Bienvenido.module.css'
 
 const studentName = 'Nombre alumno'
 const techSkills = [{ tech: 'Html', image: htmlImg }, { tech: 'Css', image: cssImg }, { tech: 'JavaScript', image: javascriptImg }, { tech: 'React', image: reactImg }, { tech: 'Redux', image: redux }]
@@ -13,10 +13,10 @@ const alerts = { m1: 'Aprobado', m2: 'En curso' }
 
 export default function Bienvenido () {
   return (
-    <div>
-      <h1>soy Henry!</h1>
-      <h3>{studentName}</h3>
-      <ul>
+    <div className='{divBienvenido}'>
+      <h1 className='{title}'>soy Henry!</h1>
+      <h3 className='{subtitle}'>{studentName}</h3>
+      <ul className='{unorderedList}'>
         {techSkills.map(skill => (
           <li key={skill}>{skill.tech}<img src={skill.image} alt={skill.tech} /></li>
         ))}
