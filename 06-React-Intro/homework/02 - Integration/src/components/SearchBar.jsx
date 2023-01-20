fb24a16f79ab202a12d077ddaeea20685d89ef16
Marcelo <gelatomarcelo@hotmail.com>
2023-01-20 17:09:05 -0300

    tag acomodado lugar

--- 06-React-Intro/homework/02 - Integration/src/components/SearchBar.jsx
+++ 06-React-Intro/homework/02 - Integration/src/components/SearchBar.jsx
@@ -2,7 +2,7 @@ export default function SearchBar(props) {
    return (
       <div>
          <input type='search' />
-      <button onClick={() => props.onSearch('un ID')}>Agregar</button>
+         <button onClick={() => props.onSearch('un ID')}>Agregar</button>
       </div>
    );
 }
