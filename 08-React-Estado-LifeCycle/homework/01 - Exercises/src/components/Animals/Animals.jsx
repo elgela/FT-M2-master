f581c0b86aa3caaf3b8b3e655462f35b97d00958
elgela <gelatomarcelo@hotmail.com>
2023-01-24 11:04:55 -0300

    incompleto

--- 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Animals/Animals.jsx
+++ 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Animals/Animals.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 
 export default class Animals extends React.Component {
-  constructor (props) {
-    super()
-  }
-
   render () {
-    return <div></div>
-  }
+    const { animals } = this.props;
+    return (
+    <div>
+      {animals?.map(animal => {
+        return(
+          <div key={animal.name}>
+            <h5>{animal.name}</h5>
+            <img src={animal.image} alt={animal.name} width="300px" />
+            <br />
+            <span>{animal.specie}</span>
+          </div>
+        )
+      })}
+    </div>
+  )}
 }
