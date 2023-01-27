6f26124f6fd3b34ceb730837262c64a496b0a9e3
elgela <gelatomarcelo@hotmail.com>
2023-01-27 00:32:22 -0300

    incompleto

--- 09-React-Routing/homework/01 - Exercises/src/App.js
+++ 09-React-Routing/homework/01 - Exercises/src/App.js
@@ -5,7 +5,7 @@ import NavBar from "./components/NavBar/NavBar.jsx";
 import Shipping from "./components/Shipping/Shipping.jsx";
 import Promotions from "./components/Promotions/Promotions.jsx";
 import CardDetail from "./components/CardDetail/CardDetail.jsx";
-import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
+import { Route, Routes, useLocation, useNavigate, } from "react-router-dom";
 /* eslint-disable */
 
 export default function App() {
